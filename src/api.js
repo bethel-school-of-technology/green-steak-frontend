990e00e2ba299c426615b3e72e69927104e03c14
Brent Hinsz <blhinsz@gmail.com>
2019-04-25 16:26:13 -0400

    Adding Values to homepage

---
+++ src/api.js
@@ -0,0 +1,13 @@
+var axios = require('axios');
+
+module.exports = {
+    fetchValues: function(url) {
+        var encodedURI = window.encodeURI('http://localhost:3001/steakhouses');
+        return axios.get(encodedURI)
+            .then(response => {
+                //console.log('response: ', response)
+                //console.log('fetchValues: ', response.data);
+                return response.data;
+            });
+    }
+};
