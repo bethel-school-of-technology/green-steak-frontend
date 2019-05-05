a33f6fbba3a68c970a8dac1e4096472ff88aa6de
BrentLH <bhinsz@hotmail.com>
2019-05-05 19:58:13 -0400

    Star Rating Integration

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,35 +1,48 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, NavLink, Link } from 'react-router-dom';
+import Rating from './rating';
+
 
 class ReviewForm extends Component {
-    render () {
+  render() {
 
-        return (
-        <Router path="/users/review">
-          <div className="App__Form">
-            <div className="PageSwitcher">
-              <NavLink to="/users/example" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">About</NavLink>
-              <NavLink exact to="/users/review" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Review</NavLink>          
-            </div>
-            <div className="FormTitle">
-              <NavLink to="/users/example" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Find Steak</NavLink> or <NavLink exact to="/users/review" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Write A Review</NavLink>
-            </div>
-            <div className="FormCenter">
-              <form onSubmit={this.handleSubmit} className="FormFields" method="POST" action="http://localhost:3000/api/users/review">
-                <div className="FormField">
-                  <label className="FormField__Label" htmlFor="review">Write a Review</label>
-                  <input type="text" id="review" className="FormField__Input" placeholder="How was your experience?" name="review" />
-                </div>
-                <div className="FormField">
-                    <button /*onClick Logic for POST request*/ className="FormField__Button mr-20">Post</button>
-                </div>
-                {/*Rating here*/}
-              </form>
-            </div>
+    return (
+      <Router path="/users/review">
+        <div className="App__Form">
+          <div className="PageSwitcher">
+            <NavLink to="/users/example" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">About</NavLink>
+            <NavLink exact to="/users/review" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Review</NavLink>
           </div>
-        </Router>
-        );
-    }
+          <div className="FormTitle">
+            <NavLink to="/users/example" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Find Steak</NavLink> or <NavLink exact to="/users/review" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Write A Review</NavLink>
+          </div>
+          <div className="FormCenter">
+            <form onSubmit={this.handleSubmit} className="FormFields" method="POST" action="http://localhost:3000/api/users/review">
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="review">Write a Review</label>
+                <input type="text" id="review" className="FormField__Input" placeholder="How was your experience?" name="review" />
+              </div>
+              <div className="FormField">
+                <button /*onClick Logic for POST request*/ className="FormField__Button mr-20">Post</button>
+              </div>
+
+              <Rating />
+              <div>
+                <p className="comments">
+                  Describe your steakhouse experience:
+            <br />
+                  <textarea rows={10} cols={50} name="rating" defaultValue={""} />
+                  <button>Submit</button>
+                </p>
+                {/* </div> */}
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 
---
+++ src/components/rating.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Rating extends Component {
+    render() {
+
+        return (
+            <div>
+                Rate Steakhouse Price 1-5 Stars:
+        <br />
+                {/* Star Rating is from https://codepen.io/tammykimkim/pen/yegZRw  
+    which was modified from https://github.com/mukulkant/Star-rating-using-pure-css */}
+                <div className="rate">
+                    <input type="radio" id="star5" name="rateprice" defaultValue={5} />
+                    <label htmlFor="star5" title="5 star">5 stars</label>
+
+                    <input type="radio" id="star4" name="rateprice" defaultValue={4} />
+                    <label htmlFor="star4" title="4 star">4 stars</label>
+
+                    <input type="radio" id="star3" name="rateprice" defaultValue={3} />
+                    <label htmlFor="star3" title="3 star">3 stars</label>
+
+                    <input type="radio" id="star2" name="rateprice" defaultValue={2} />
+                    <label htmlFor="star2" title="2 star">2 stars</label>
+
+                    <input type="radio" id="star1" name="rateprice" defaultValue={1} />
+                    <label htmlFor="star1" title="1 star">1 star</label>
+                </div>
+                <br />
+                <br />
+                <br />
+            
+                Rate Steakhouse Quality 1-5 Stars:
+                <br />
+            
+                {/* Star Rating is from https://codepen.io/tammykimkim/pen/yegZRw  
+    which was modified from https://github.com/mukulkant/Star-rating-using-pure-css */}
+                <div className="rate">
+                    <input type="radio" id="star10" name="rateQuality" defaultValue={5} />
+                    <label htmlFor="star10" title="5 star">5 stars</label>
+
+                    <input type="radio" id="star9" name="rateQuality" defaultValue={4} />
+                    <label htmlFor="star9" title="4 star">4 stars</label>
+
+                    <input type="radio" id="star8" name="rateQuality" defaultValue={3} />
+                    <label htmlFor="star8" title="3 star">3 stars</label>
+
+                    <input type="radio" id="star7" name="rateQuality" defaultValue={2} />
+                    <label htmlFor="star7" title="2 star">2 stars</label>
+
+                    <input type="radio" id="star6" name="rateQuality" defaultValue={1} />
+                    <label htmlFor="star6" title="1 star">1 star</label>
+                </div>
+                <br />
+            </div>
+        );
+    }
+};
+export default Rating;
