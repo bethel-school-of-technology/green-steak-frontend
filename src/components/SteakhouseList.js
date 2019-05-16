4e32bed13ec7b20df4df6e7a67906c482b878032
Unknown <jessyrivard@icloud.com>
2019-05-16 17:16:21 -0400

    Improved Mobile View

--- src/components/SteakhouseList.js
+++ src/components/SteakhouseList.js
@@ -7,7 +7,7 @@ import * as auth from "../auth";
 
 class SteakhouseList extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       steakhouseList: []
@@ -15,19 +15,18 @@ class SteakhouseList extends Component {
   }
 
   componentWillMount() {
-    auth.auth()
-    .then(isLogged => {
+    auth.auth().then(isLogged => {
       console.log(isLogged);
       if (isLogged.loggedIn === false) {
         alert("Error: Not logged in. Please log in.");
         window.location.href = "#/users/sign-in";
       }
     });
-}
+  }
 
   componentDidMount() {
     this.callValues();
-    }
+  }
 
   callValues = () => {
     api
@@ -51,9 +50,7 @@ class SteakhouseList extends Component {
     return (
       <Router path="/steakhouses/info">
         <div>
-          <Map 
-            steakhouseList={this.state.steakhouseList}
-          />
+          <Map steakhouseList={this.state.steakhouseList} />
           <div className="Value">
             {steakhouseList &&
               steakhouseList.length > 0 &&
@@ -68,6 +65,24 @@ class SteakhouseList extends Component {
                   {val.name}
                 </button>
               ))}
+            {error && <h1>Error!</h1>}
+          </div>
+          <div className="ValueMobile">
+            <select
+              className="drpdwn"
+              onChange={function(event) {
+                window.location.href = "#/steakhouses/info/" + event.target.value ;
+              }}
+            >
+              {steakhouseList &&
+                steakhouseList.length > 0 &&
+                steakhouseList.map(val => (
+                  <option key={val._id} value={val._id}>
+                    {val.name}
+                  </option>
+                ))}
+            </select>
+             <span className="drpdwnArrow">▼</span>
             {error && <h1>Error!</h1>}
           </div>
         </div>
